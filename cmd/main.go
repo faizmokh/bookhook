4f64ca14c8052d9fca95a58a9d564f99c2ff3e3d
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-09-21 11:02:39 +0800

    fix: move godotenv to main

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 
 	hooks "github.com/faizmokhtar/bookhook"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error: unable to load .env file")
+	}
+
 	http.HandleFunc("/", hooks.TwitterWebhook)
 	log.Println("Listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- hooks.go
+++ hooks.go
@@ -2,20 +2,12 @@ package hooks
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func TwitterWebhook(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Yo!")
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error: unable to load .env file")
-	}
 
 	_ = os.Getenv("ACCESS_TOKEN")
 	_ = os.Getenv("ACCESS_TOKEN_SECRET")
